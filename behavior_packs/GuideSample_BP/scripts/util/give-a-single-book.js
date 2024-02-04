f293da232b366b708e8da4b427807015725d22ec
jigarbov <30274167+jigarbov@users.noreply.github.com>
2024-02-04 17:08:19 +1100

    updated stuff

--- behavior_packs/GuideSample_BP/scripts/util/give-a-single-book.js
+++ behavior_packs/GuideSample_BP/scripts/util/give-a-single-book.js
@@ -3,12 +3,13 @@ import { world,ItemStack } from "@minecraft/server";
 const giveASingleBook = ({
 	itemTypeId
   }) => {
+	let guide_checker = itemTypeId+"_given"
 	world.afterEvents.playerSpawn.subscribe(async (event) => {
-		if(event.player.getDynamicProperty(itemTypeId) === undefined){
+		if(event.player.getDynamicProperty(guide_checker) === undefined){
 			let playerpos = event.player.location
-			let item = new ItemStack(itemTypeId,1)
+			let item = new ItemStack(guide_checker,1)
 			event.player.dimension.spawnItem(item, playerpos)
-			event.player.setDynamicProperty(itemTypeId, 1);
+			event.player.setDynamicProperty(guide_checker, 1);
 		}
 	});
   };
