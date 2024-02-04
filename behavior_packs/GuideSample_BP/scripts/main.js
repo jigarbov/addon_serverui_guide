068270d404d90ec793b7e7ffe9b30e0d3d265ba2
jigarbov <30274167+jigarbov@users.noreply.github.com>
2024-02-04 16:29:16 +1100

    some changes

--- behavior_packs/GuideSample_BP/scripts/main.js
+++ behavior_packs/GuideSample_BP/scripts/main.js
@@ -1,16 +1,18 @@
 import showDialogueOnItemUse from "./util/show-dialogue-on-item-use";
 import giveASingleBook from "./util/give-a-single-book";
 
-const BOOK_ID = "studioname_packname:guide" // Change this to be the name of your book item.
+const BOOK_ID = "studioname_packname:guide" // If you did a global find and replace, this should now be the same as your book item.
 
 showDialogueOnItemUse({
   itemTypeId: BOOK_ID,
   title: {
-    translate: "studioname_packname:guide.name", // Change this to be the lang string that should appear as the title of the dialogue window
+    translate: "studioname_packname:guide.name", // If you did a global find and replace, this should now be the same as lang file already.
   },
   content: {
     rawtext: [
-      {translate: "studioname_packname:info.part_01",with: ["\n\n"],}, // Change these to be the lang strings that should appear in your ServerUI. The formatting is standard rawtext and can have as many entries as you need.
+	// Change these to be the lang strings that should appear in your ServerUI.
+	// The formatting is standard rawtext and can have as many entries as you need, but keep it concise!!
+      {translate: "studioname_packname:info.part_01",with: ["\n"],}, 
       {text: "§r\n\n",},
       {translate: "studioname_packname:info.part_02",with: ["\n"],},
       {text: "§r\n\n",},
@@ -18,15 +20,17 @@ showDialogueOnItemUse({
       {text: "§r\n\n",},
       {translate: "studioname_packname:info.part_04",with: ["\n"],},
       {text: "§r\n\n",},
-      {translate: "studioname_packname:info.part_05",with: ["\n","\n","\n","\n","\n","\n\n","\n"],},
+      {translate: "studioname_packname:info.part_05",with: ["\n"],},
       {text: "§r\n\n",},
       {translate: "studioname_packname:info.part_06",with: ["\n"],},
       {text: "§r\n\n",},
-      {translate: "studioname_packname:info.part_07",with: ["\n\n", "\n\n"],},
+      {translate: "studioname_packname:info.part_07",with: ["\n"],},
+      {text: "§r\n\n",},
+      {translate: "studioname_packname:info.part_08",with: ["\n"],},
     ],
   },
   exit: {
-    translate: "studioname_packname:guide_exit", // Change this to be your lang string for the button on the bottom of the window.
+    translate: "studioname_packname:guide_exit", // If you did a global find and replace, this should now be the same as lang file already.
   },
   soundOnOpen: "mob.zombie.say", // Remove or change this line to your custom sounds
   soundOnClose: "mob.chicken.say", // Remove or change this line to your custom sounds
